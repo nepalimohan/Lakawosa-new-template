6b7c9c5230258f7eb5bf08fbc05bc7db8e0bd241
nepalimohan <nepalimohan@outlook.com>
2022-07-11 13:28:30 +0545

    Modifications done

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# class Products(models.Model):
+#     product_name = models.CharField(max_length=100)
+#     price = models.IntegerField()
+#     product_type = 
